d77ac32c6aeebeef8136e5570fe89eb72d8638fc
Dat Proto <contact@datproto.com>
2023-01-23 18:44:17 +0700

    DOING: Sanity setup

---
+++ sanity/schemas/restaurants.js
@@ -0,0 +1,67 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'restaurant',
+  title: 'Restaurant',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation: (Rule) => Rule.required()
+    }),
+    defineField({
+      name: 'short_description',
+      title: 'Short Description',
+      type: 'string',
+      validation: (Rule) => Rule.max(200)
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image'
+    }),
+    defineField({
+      name: 'long',
+      title: 'Longitude',
+      type: 'number'
+    }),
+    defineField({
+      name: 'lat',
+      title: 'latitude',
+      type: 'number'
+    }),
+    defineField({
+      name: 'address',
+      title: 'Address',
+      type: 'string',
+      validation: (Rule) => Rule.required()
+    }),
+    defineField({
+      name: 'rating',
+      title: 'Enter a Rating from (1-5 Stars)',
+      type: 'number',
+      validation: (Rule) => Rule.required()
+        .min(1)
+        .max(5)
+        .error('Please enter a Value between 1 and 5')
+    }),
+    defineField({
+      name: 'type',
+      title: 'Category',
+      type: 'reference',
+      validation: (Rule) => Rule.required(),
+      to: [{type: 'category'}]
+    }),
+    defineField({
+      name: 'dishes',
+      title: 'Dishes',
+      type: 'array',
+      of: [{
+        type: 'reference',
+        to: [{ type: 'dish' }]
+      }]
+    }),
+  ]
+})
